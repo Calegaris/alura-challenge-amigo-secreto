3d7e28fe3ad8c09d663407a28fe9b9ffa74e5cdc
Angel <lcalegari97@gmail.com>
2025-02-02 03:35:34 -0300

    creada la lista, la funcion agregarAmigo y algunas funcionalidades extra, agregada tambien lineas de codigo en el archivo html y css

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombres = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+
+    if (nombre.trim() === '') {
+        alert('El campo no puede estar vacio, /nPor favor, inserte un nombre.');
+        return;
+    } else if (nombres.includes(nombre)) {
+        alert('El nombre ya fue agregado, por favor, inserte otro nombre.');
+        return;
+    } else {
+        nombres.push(nombre);
+        console.log(nombres); //para ver si se estan agregando los nombres
+
+        // Muestra el mensaje de éxito en el HTML
+        document.getElementById('mensaje-exito').innerText = "Nombre agregado exitosamente";
+        setTimeout(() => {
+            document.getElementById('mensaje-exito').innerText = ""; // Elimina el mensaje después de 5 segundos
+        }, 3000);
+
+        document.getElementById('amigo').value = "";
+    }
+}
